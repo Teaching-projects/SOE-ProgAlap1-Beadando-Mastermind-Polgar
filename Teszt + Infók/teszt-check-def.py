a8a6ab6d7f564c9865a654f94d7b7f436e8246f1
Teo2002 <71121136+Teo2002@users.noreply.github.com>
2021-01-18 21:00:08 +0100

    Update teszt-check-def.py

--- Teszt + Infók/teszt-check-def.py
+++ Teszt + Infók/teszt-check-def.py
@@ -1,1 +1,31 @@
+from alldef_mm import load_setup as cf
+from alldef_mm import general as general
 
+beallitasok = "mastermind.setup"
+adatbazis = "mastermind.adatok"
+
+conf = cf(beallitasok)
+feladvany = general(conf, adatbazis)
+print(feladvany)
+
+bevitel = []
+for i in range(conf[1]):
+    bevitel.append(int(input()))
+
+
+
+def check(bevitel, feladvany, conf):
+    checktipp = []
+    for i in range(conf[1]):
+        if bevitel[i] == feladvany[i]:
+            checktipp.append(" OK ")
+        elif bevitel[i] in feladvany: 
+            checktipp.append(" RH ")
+        else:
+            checktipp.append(" NO ")
+    if checktipp.count(" OK ") < conf[1]:
+        return checktipp
+    else:
+        return True
+
+print(check(bevitel, feladvany, conf))
